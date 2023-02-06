b4f41eda58a48e2efb9fb423b75148e90d9b3c72
Adil Parwez <dlprwz@gmail.com>
2023-02-07 04:52:06 +0530

    added calender

--- lib/ui/add_task_bar.dart
+++ lib/ui/add_task_bar.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, no_leading_underscores_for_local_identifiers, unnecessary_import, avoid_unnecessary_containers, prefer_final_fields, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -15,52 +17,113 @@ class AddTaskPage extends StatefulWidget {
 
 class _AddTaskPageState extends State<AddTaskPage> {
   DateTime _selectedDate = DateTime.now();
+  String _endTime = '09:30 PM';
+  String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.theme.backgroundColor,
       appBar: _appBar(context),
       body: SingleChildScrollView(
-        padding: EdgeInsets.only(left: 20,right: 20),
-        child: Container(child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Add Task",
-            style: headingStyle,),
-            MyInputField(title: 'Title', hint: 'Enter Title',),
-            MyInputField(title: 'Note', hint: 'Enter your note',),
-            MyInputField(title: 'Date', hint: DateFormat.yMd().format(_selectedDate),
-            widget: IconButton( icon: Icon(Icons.calendar_month_outlined),
-              onPressed: () {
-
-              },),)
-
-
-
-          ],
-        ),),
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Add Task",
+                style: headingStyle,
+              ),
+              MyInputField(
+                title: 'Title',
+                hint: 'Enter Title',
+              ),
+              MyInputField(
+                title: 'Note',
+                hint: 'Enter your note',
+              ),
+              MyInputField(
+                title: 'Date',
+                hint: DateFormat.yMd().format(_selectedDate),
+                widget: IconButton(
+                  icon: Icon(Icons.calendar_month_outlined, color: Colors.grey),
+                  onPressed: () {
+                    _getDateFromUser();
+                  },
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: MyInputField(
+                      title: 'Time',
+                      hint: _startTime,
+                      widget: IconButton(
+                        icon:
+                            Icon(Icons.access_time_rounded, color: Colors.grey),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: MyInputField(
+                      title: 'Time',
+                      hint: _startTime,
+                      widget: IconButton(
+                        icon:
+                            Icon(Icons.access_time_rounded, color: Colors.grey),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
-}
+
 //workflow check
-_appBar(BuildContext context) {
-  return AppBar(
-    elevation: 0,
-    backgroundColor: context.theme.backgroundColor,
-    leading: GestureDetector(
-      onTap: (){
-        Get.back();
-      },
-      child:Icon(Icons.arrow_back_ios,
-        size: 20,
-        color:Get.isDarkMode?Colors.white:Colors.black,),
-    ),
-    actions: const [
-      CircleAvatar(
-        backgroundImage: AssetImage("images/vit.png"),
+  _appBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: context.theme.backgroundColor,
+      leading: GestureDetector(
+        onTap: () {
+          Get.back();
+        },
+        child: Icon(
+          Icons.arrow_back_ios,
+          size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
+        ),
       ),
-      SizedBox(width: 20,)
-    ],
-  );
+      actions: const [
+        CircleAvatar(
+          backgroundImage: AssetImage("images/vit.png"),
+        ),
+        SizedBox(
+          width: 20,
+        )
+      ],
+    );
+  }
+
+  _getDateFromUser() async {
+    DateTime? _pickerDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2010),
+      lastDate: DateTime(2123),
+    );
+
+    if (_pickerDate != null) {
+      setState(() {
+        _selectedDate = _pickerDate;
+      });
+    }
+  }
 }
